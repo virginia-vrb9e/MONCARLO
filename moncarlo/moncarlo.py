1402388650b304ec8b2d6f46fd516abada17bc7d
VirginiaRuth_Brame <vrb9e@virginia.edu>
2025-04-29 14:41:13 -0400

    .

--- moncarlo/moncarlo.py
+++ moncarlo/moncarlo.py
@@ -32,7 +32,7 @@ class MonCarloDevice:
         current_state: 
               returns the die's current state as a pandas dataframe
     """
-    img = np.array(PIL.Image.open(urllib.request.urlopen(https://images.metmuseum.org/CRDImages/eg/original/10.130.1158_001.jpg)))
+    img = np.array(PIL.Image.open(urllib.request.urlopen('https://images.metmuseum.org/CRDImages/eg/original/10.130.1158_001.jpg')))
     plt.imshow(img)
     print("THE MET Museum: \n\nicosahedron with faces inscribed with Greek letters \nPtolemaic Period–Roman Period\n(2nd century \
     B.C.– 4th century A.D.)\n")
